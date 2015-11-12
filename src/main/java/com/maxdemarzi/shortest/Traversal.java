d4f5435859981dbdc610e7d819d3c5a1cc988f92
Edd King <edd@academia.edu>
2015-11-11 16:55:46 -0800

    document the NodeCallback.explored method, specifically that the NodeItem can be null if the traversal didnt load it

--- src/main/java/com/maxdemarzi/shortest/Traversal.java
+++ src/main/java/com/maxdemarzi/shortest/Traversal.java
@@ -11,6 +11,14 @@ public abstract class Traversal {
     }
 
     public static interface NodeCallback {
+        /**
+         * @param traversal: the traversal that did the exploring (the same callback can be given to multiple traversals)
+         * @param node: The node that was explored, can be null if the traversal didnt load that node,
+         * in that case it is up to the implementer to load the node from the nodeId
+         * @param nodeId: the id of the explored node
+         * @param cost: the cost to get to this node from the traversal's starting point, in a Breadth first search this will be the depth
+         * @param paths: the number of same-cost paths to this node from the starting nodes
+         */
         public void explored(final Traversal traversal, final NodeItem node, final long nodeId, final int cost, int paths);
     }
 
